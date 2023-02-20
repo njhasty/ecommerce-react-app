a7ba64662641e382d759a789df3e4f4ccdea9b58
Nick <nicholas.hasty1@gmail.com>
2023-02-20 06:25:13 -0500

    Add files via upload

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -9,7 +9,7 @@ export const Product = (props) => {
 
   return (
     <div className="product">
-      <img src={productImage} />
+      <img src={productImage} alt="Product Image"/>
       <div className="description">
         <p>
           <b>{productName}</b>
